cd29cf2ffeef6cb77d81af631734c40f48bafee3
pooja tipale <poojatipale332@gmail.com>
2021-02-24 12:27:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const port = 3100
+mongoose.connect('mongodb+srv://PoojaTipale:PoojaTipale@cluster0.qfs2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+ {useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex:true
+}).then(()=> {
+    console.log("DB CONNECTED")
+})
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My routes
+const categoryRoutes = require("./routes/category");
+
+//My Routes
+
+app.use("/api", categoryRoutes);
+
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
